88cd17596edc89ced552fd1db50f364e21d79d3e
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-21 12:42:34 +0100

    add: button group comp

---
+++ src/components/buttons/ButtonGroup.jsx
@@ -0,0 +1,24 @@
+
+import languages from "../../data/languages";
+import Button from "./Button";
+
+
+export default function ButtonGroup () {
+
+    return (
+
+        <>
+            <div className="flex flex-wrap justify-center items-center gap-4">
+
+                {/* ciclo languages per btn comp */}
+                {
+                    languages.map( (language) => {
+                        <Button key={language.id} lang={language} />
+                    } )
+                }
+            </div>
+        </>
+
+    )
+
+}
